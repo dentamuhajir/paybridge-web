00f82685ffd896d923efab79a8ff86970f22939c
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-11-25 20:23:33 +0700

    Implement middleware to protected components

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,26 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import RtlLayout from "layouts/rtl";
 import AdminLayout from "layouts/admin";
 import AuthLayout from "layouts/auth";
+
+// Middleware
+import ProtectedRoute from "./middleware/ProtectedRoute";
+
 const App = () => {
   return (
     <Routes>
-      <Route path="auth/*" element={<AuthLayout />} />
-      <Route path="admin/*" element={<AdminLayout />} />
+      {/* PUBLIC ROUTES */}
+      <Route path="/auth/*" element={<AuthLayout />} />
+
+      {/* PROTECTED ROUTES */}
+      <Route 
+        path="admin/*" 
+        element={
+          <ProtectedRoute>
+            <AdminLayout />
+          </ProtectedRoute>
+        } 
+      />
+      
       <Route path="rtl/*" element={<RtlLayout />} />
       <Route path="/" element={<Navigate to="/admin" replace />} />
     </Routes>
