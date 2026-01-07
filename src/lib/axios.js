a2e83c69bd6492b9534b1ce18191b0dd36be8b85
Denta Muhajir <denta.muhajir@binus.ac.id>
2026-01-08 01:04:29 +0700

    Add bearer token in axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,11 +1,27 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_USER_API_URL,
-    timeout: 10000,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: process.env.REACT_APP_USER_API_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+/**
+ * Attach Bearer token to every request
+ */
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
